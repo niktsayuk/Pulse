68af20e062a45474104dd8ca071b1a3b9789a839
nstsayuk <niktsayuk@outlook.com>
2023-08-28 22:30:31 +1000

    add slider and library Slick

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1000,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left-arrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right-arrow.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+  });
